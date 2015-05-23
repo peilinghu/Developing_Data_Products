4a64e47ce16af0299849b05ba4b8e93d69400a70
peilinghu <peiling_hu@yahoo.com>
2015-05-23 02:53:59 -0700

    update

--- ui.R
+++ ui.R
@@ -6,24 +6,27 @@ shinyUI(fluidPage(
                 titlePanel("Developing Data Products - Gas mileage estimation"),
 
                 sidebarPanel(
-                        numericInput('wt', label='Weight of car(in 1000 lbs)', value=1, min=0, max=6, step=0.1),
-                        sliderInput(inputId="hp", label=h4("Horse power"), min=50, max=300, value=100),
-                        radioButtons(inputId="am", label=h4("Transmission"),
+                                p("Input the vehicle weight, horse power, and select the 
+                                   transmission type, click Estimate button. Estimated MPG is indicated 
+                                   in the Estimated miles per gallon field at the right panel."),
+                                numericInput('wt', label=h5('Weight of car(in 1000 lbs)'), value=1, min=0, max=6, step=0.1),
+                                sliderInput(inputId="hp", label=h5("Horse power"), min=50, max=300, value=100),
+                                radioButtons(inputId="am", label=h5("Transmission"),
                                      choices = list("Automatic" = 0, "Manual" = 1), selected=0),
                         
-                        submitButton("Estimate!")
-                ),
+                                submitButton("Estimate!")
+                        ),
                 
                 mainPanel(
-                        h5('Weight in 1000 lbs'),
-                        verbatimTextOutput( 'wt'),
-                        h5('Horse power'),
-                        verbatimTextOutput( 'hp' ),
-                        h5('Transmission'),
-                        verbatimTextOutput( 'am' ),
-                        h5('Estimated miles per gallon'),
-                        verbatimTextOutput( 'prediction' )
-                )
-        )
-
+                                h4('Weight entered'),
+                                verbatimTextOutput( 'wt'),
+                                h4('Horse power entered'),
+                                verbatimTextOutput( 'hp' ),
+                                h4('Transmission selected'),
+                                verbatimTextOutput( 'am' ),
+                                h4('Estimated miles per gallon'),
+                                verbatimTextOutput( 'prediction') 
+                                )        
+        )               
 )
+        
